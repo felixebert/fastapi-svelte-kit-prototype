78313a5d79064f7b98ddc98bdedd6c5a91145754
Felix Ebert <felix.ebert@if-core.de>
2021-07-22 05:20:11 +0200

    add support for nested css definitions, add css autoprefixer

--- ui/svelte.config.js
+++ ui/svelte.config.js
@@ -1,10 +1,16 @@
 import preprocess from 'svelte-preprocess';
+import autoprefixer from 'autoprefixer';
+import postcssNested from 'postcss-nested';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: preprocess({
+		postcss: {
+			plugins: [postcssNested(), autoprefixer()]
+		}
+	}),
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
