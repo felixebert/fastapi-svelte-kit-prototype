ea0f2646cffd47b1e6cdd8867ab006ab3dfe3dce
Felix Ebert <felix.ebert@if-core.de>
2021-07-26 22:59:41 +0200

    env var for svelte base path to allow deployment to subdirectory

--- ui/svelte.config.js
+++ ui/svelte.config.js
@@ -16,7 +16,10 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: adapter()
+		adapter: adapter(),
+		paths: {
+			base: process.env.BASE_PATH || ''
+		}
 	}
 };
 
