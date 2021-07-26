3fe0c968af2ebc0fd0175f71b6d13147901eeb68
Felix Ebert <felix.ebert@if-core.de>
2021-07-26 20:22:35 +0200

    add cache header for users/me (disable cache)

--- app/src/routers/users.py
+++ app/src/routers/users.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, APIRouter
+from fastapi import Depends, APIRouter, Response
 from ..lib.schemas import User
 from ..lib.dependencies import get_current_user
 
@@ -9,5 +9,6 @@ router = APIRouter(
 
 
 @router.get("/me")
-async def read_users_me(current_user: User = Depends(get_current_user)):
+async def read_users_me(response: Response, current_user: User = Depends(get_current_user)):
+    response.headers["Cache-Control"] = "no-cache, max-age=0"
     return current_user
